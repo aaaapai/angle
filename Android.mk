27f80c7171ab8e5c83e447f16a8e3ff874cd32ae
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-04-27 15:25:45 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,2 +1,3 @@
 # This file exists purely to prevent Android.mk files in subdirectories from being automatically used
 # See http://b/188491905 for more details
+LOCAL_CFLAGS += -DANDROID -pipe -integrated-as -fno-plt -Ofast -flto -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce
