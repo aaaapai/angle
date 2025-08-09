5c83e47e352e577b6a1737b2a082837591ed06a8
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-10 01:26:34 +0800

    Update validationES3.cpp

--- src/libANGLE/validationES3.cpp
+++ src/libANGLE/validationES3.cpp
@@ -247,13 +247,13 @@ bool ValidateTexImageFormatCombination(const Context *context,
         // The type and format are valid if any supported internal format has that type and format
         if (!ValidDesktopFormat(format))
         {
-            context->validationError(GL_INVALID_ENUM, kInvalidFormat);
+            errors->validationError(GL_INVALID_ENUM, kInvalidFormat);
             return false;
         }
 
         if (!ValidDesktopType(type))
         {
-            context->validationError(GL_INVALID_ENUM, kInvalidType);
+            errors->validationError(GL_INVALID_ENUM, kInvalidType);
             return false;
         }
     }
