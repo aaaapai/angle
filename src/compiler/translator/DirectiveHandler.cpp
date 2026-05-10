332ad8e04c3cd506919134cb6438d197ddce375e
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-05-10 15:53:25 +0800

    Update DirectiveHandler.cpp

--- src/compiler/translator/DirectiveHandler.cpp
+++ src/compiler/translator/DirectiveHandler.cpp
@@ -309,7 +309,7 @@ void TDirectiveHandler::handleVersion(const angle::pp::SourceLocation &loc,
                                       angle::pp::MacroSet *macro_set)
 {
 
-    if (version == 100 || version == 110 || version == 120 || version == 150 || version == 300 || version == 310 || version == 320)
+    if (version == 100 || version == 110 || version == 120 || version == 150 || version == 300 || version == 310 || version == 320 || version == 330)
     {
         mContext.onShaderVersionDeclared(version);
 
