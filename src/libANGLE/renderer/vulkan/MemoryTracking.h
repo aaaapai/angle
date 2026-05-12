75027d73b02748f378047dee78c1ba9b065423b9
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-05-12 23:17:39 +0800

    Update MemoryTracking.h

--- src/libANGLE/renderer/vulkan/MemoryTracking.h
+++ src/libANGLE/renderer/vulkan/MemoryTracking.h
@@ -19,6 +19,9 @@
 #include "common/hash_containers.h"
 #include "common/vulkan/vk_headers.h"
 
+#include <ankerl/stl.h>
+#include <ankerl/unordered_dense.h>
+
 namespace rx
 {
 namespace vk
@@ -224,7 +227,7 @@ class MemoryAllocationTracker : angle::NonCopyable
     // allocation ID and a record of all active allocations.
     uint64_t mMemoryAllocationID;
     using MemoryAllocInfoMap = angle::HashMap<vk::MemoryAllocInfoMapKey, vk::MemoryAllocationInfo>;
-    std::unordered_map<angle::BacktraceInfo, MemoryAllocInfoMap> mMemoryAllocationRecord;
+    ankerl::unordered_dense::map<angle::BacktraceInfo, MemoryAllocInfoMap> mMemoryAllocationRecord;
 };
 }  // namespace rx
 
