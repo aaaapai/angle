156883ce81535ec79c0951fd200bc12c6c77796b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-05-17 00:39:03 +0800

    Update Program.cpp

--- src/libANGLE/Program.cpp
+++ src/libANGLE/Program.cpp
@@ -1774,7 +1774,7 @@ bool Program::linkValidateShaders()
         if (!shaders[ShaderType::Compute]->localSize.isDeclared())
         {
             mState.mInfoLog << "Work group size is not specified.";
-            //return false;
+            if (!std::getenv("ANGLE_ALLOW_UNSPECIFIED_WORK_GROUP_SIZE")) return false;
         }
     }
     else
