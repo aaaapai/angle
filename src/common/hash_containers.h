ee2f9d36b1e8c22de76798173fcec117bc4a638b
aaaapai <mcnb66666@yeah.net>
2026-08-25 12:48:42 +0800

    Faster?

--- src/common/hash_containers.h
+++ src/common/hash_containers.h
@@ -9,16 +9,13 @@
 #ifndef COMMON_HASH_CONTAINERS_H_
 #define COMMON_HASH_CONTAINERS_H_
 
-//#undef ANGLE_USE_ABSEIL // Is ABSEIL faster?
-
 #if defined(ANGLE_USE_ABSEIL)
 #    include "absl/container/flat_hash_map.h"
 #    include "absl/container/flat_hash_set.h"
 #else
 #    include <unordered_map>
 #    include <unordered_set>
-#    include <ankerl/stl.h>
-#    include <ankerl/unordered_dense.h>
+#    include <ska/flat_hash_map.hpp>
 #endif  // defined(ANGLE_USE_ABSEIL)
 
 namespace angle
@@ -27,9 +24,9 @@ namespace angle
 #if defined(ANGLE_USE_ABSEIL)
 template <typename Key,
           typename T,
-          class Hash = absl::container_internal::hash_default_hash<Key>,
-          class Eq   = absl::container_internal::hash_default_eq<Key>>
-using HashMap = absl::flat_hash_map<Key, T, Hash, Eq>;
+          class Hash = std::hash<Key>,
+          class Eq   = std::equal_to<Key>>
+using HashMap = ska::flat_hash_map<Key, T, Hash, Eq>;
 template <typename Key,
           class Hash = absl::container_internal::hash_default_hash<Key>,
           class Eq   = absl::container_internal::hash_default_eq<Key>>
@@ -44,7 +41,7 @@ template <typename Key,
           class KeyEqual = std::equal_to<Key>>
 using HashMap = ankerl::unordered_dense::map<Key, T, Hash, KeyEqual>;
 template <typename Key, class Hash = std::hash<Key>, class KeyEqual = std::equal_to<Key>>
-using HashSet = ankerl::unordered_dense::set<Key, Hash, KeyEqual>;
+using HashSet = std::unordered_set<Key, Hash, KeyEqual>;
 #    if __cpp_lib_generic_unordered_lookup >= 201811L
 #        define ANGLE_HAS_HASH_MAP_GENERIC_LOOKUP 1
 #    else
